b90d15d81d3ac4f9dce1ecf9e6015394c804fdcf
Ameer Hasan <ameer.hasan.dev@gmail.com>
2024-09-28 12:24:58 -0400

    Refactor tutorial page component

--- app/(pages)/tutorial/[id]/page.jsx
+++ app/(pages)/tutorial/[id]/page.jsx
@@ -4,7 +4,6 @@ import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import { auth, firestore } from "@/utils/firebase";
 import { doc, getDoc, setDoc } from "firebase/firestore";
-import ProtectedRoute from "@/app/components/ProtectedRoute";
 import { motion } from "framer-motion";
 import { Spinner } from "@/app/components/Spinner";
 
@@ -105,65 +104,63 @@ export default function TutorialDetailsPage({ params }) {
   }
 
   return (
-    <ProtectedRoute>
-      <div className="flex max-w-7xl mx-auto p-8 bg-white shadow-lg rounded-lg mt-10">
-        <div className="w-1/4 p-4 border-r border-gray-300">
-          <h2 className="text-xl font-semibold mb-4 text-gray-800">
-            Tutorial Steps
-          </h2>
-          {tutorial.steps && tutorial.steps.length > 0 && (
-            <ul>
-              {tutorial.steps.map((step, index) => (
-                <li
-                  key={index}
-                  className={`mb-2 cursor-pointer p-2 rounded-md ${
-                    index === selectedStep
-                      ? "bg-blue-500 text-white"
-                      : "bg-gray-100 text-gray-800"
-                  }`}
-                  onClick={() => setSelectedStep(index)}
-                >
-                  Step {index + 1}: {step.title}
-                </li>
-              ))}
-            </ul>
-          )}
-        </div>
-        <div className="w-3/4 p-4">
-          <h1 className="text-3xl font-bold mb-4 text-center text-gray-800">
-            {tutorial.title}
-          </h1>
-          <p className="text-lg mb-4 text-gray-800">{tutorial.description}</p>
-          {tutorial.steps && tutorial.steps.length > 0 && (
-            <motion.div
-              initial={{ opacity: 0, y: 50 }}
-              animate={{ opacity: 1, y: 0 }}
-              transition={{ duration: 0.5 }}
-              className="mb-4"
-            >
-              <h2 className="text-xl font-semibold mb-2 text-gray-800">
-                Step {selectedStep + 1}: {tutorial.steps[selectedStep].title}
-              </h2>
-              <pre className="bg-gray-100 p-4 rounded-md overflow-x-auto">
-                <code>{tutorial.steps[selectedStep].codeSnippet}</code>
-              </pre>
-              <button
-                onClick={() => toggleStepCompletion(selectedStep)}
-                className={`mt-2 bg-green-500 text-white px-4 py-2 rounded-md shadow-md hover:bg-green-600 focus:ring-2 focus:ring-green-500 focus:ring-opacity-50`}
+    <div className="flex max-w-7xl mx-auto p-8 bg-white shadow-lg rounded-lg mt-10">
+      <div className="w-1/4 p-4 border-r border-gray-300">
+        <h2 className="text-xl font-semibold mb-4 text-gray-800">
+          Tutorial Steps
+        </h2>
+        {tutorial.steps && tutorial.steps.length > 0 && (
+          <ul>
+            {tutorial.steps.map((step, index) => (
+              <li
+                key={index}
+                className={`mb-2 cursor-pointer p-2 rounded-md ${
+                  index === selectedStep
+                    ? "bg-blue-500 text-white"
+                    : "bg-gray-100 text-gray-800"
+                }`}
+                onClick={() => setSelectedStep(index)}
               >
-                {stepCompleted[selectedStep]
-                  ? "Mark as Incomplete"
-                  : "Mark as Completed"}
-              </button>
-            </motion.div>
-          )}
-          <div className="text-center mt-4">
-            <p className="text-xl text-gray-800">
-              Progress: {progress.toFixed(2)}%
-            </p>
-          </div>
+                Step {index + 1}: {step.title}
+              </li>
+            ))}
+          </ul>
+        )}
+      </div>
+      <div className="w-3/4 p-4">
+        <h1 className="text-3xl font-bold mb-4 text-center text-gray-800">
+          {tutorial.title}
+        </h1>
+        <p className="text-lg mb-4 text-gray-800">{tutorial.description}</p>
+        {tutorial.steps && tutorial.steps.length > 0 && (
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            className="mb-4"
+          >
+            <h2 className="text-xl font-semibold mb-2 text-gray-800">
+              Step {selectedStep + 1}: {tutorial.steps[selectedStep].title}
+            </h2>
+            <pre className="bg-gray-100 p-4 rounded-md overflow-x-auto">
+              <code>{tutorial.steps[selectedStep].codeSnippet}</code>
+            </pre>
+            <button
+              onClick={() => toggleStepCompletion(selectedStep)}
+              className={`mt-2 bg-green-500 text-white px-4 py-2 rounded-md shadow-md hover:bg-green-600 focus:ring-2 focus:ring-green-500 focus:ring-opacity-50`}
+            >
+              {stepCompleted[selectedStep]
+                ? "Mark as Incomplete"
+                : "Mark as Completed"}
+            </button>
+          </motion.div>
+        )}
+        <div className="text-center mt-4">
+          <p className="text-xl text-gray-800">
+            Progress: {progress.toFixed(2)}%
+          </p>
         </div>
       </div>
-    </ProtectedRoute>
+    </div>
   );
 }
